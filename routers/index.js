3dd731162fc4dfa1a0d07af5a7a0e8eae0f07b2c
GJuniarto <geryjuniarto99@gmail.com>
2023-11-17 15:38:33 +0700

    quotes feature done

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
+const QuoteController = require("../controllers/QuoteController");
 const UserController = require("../controllers/UserController");
 
 router.post("/register", UserController.register);
 
 router.post("/login", UserController.login);
+
+router.get("/quotes", QuoteController.generateQuote);
 
 module.exports = router;
