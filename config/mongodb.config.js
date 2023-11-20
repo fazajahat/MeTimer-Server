8292473c80ea27f883ceec2a6f6eb8d66d0a3243
GJuniarto <geryjuniarto99@gmail.com>
2023-11-20 17:01:24 +0700

    add: mongodb_uri

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -1,24 +1,24 @@
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb://127.0.0.1:27017/";
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri);
 
 let database = {};
 
 async function connectDB() {
-  try {
-    await client.connect();
-    database = client.db("MeTimer");
-    console.log("Database connected successfully");
-    return database;
-  } catch (err) {
-    console.log("Error connecting to database", err);
-  }
+    try {
+        await client.connect();
+        database = client.db("MeTimer");
+        console.log("Database connected successfully");
+        return database;
+    } catch (err) {
+        console.log("Error connecting to database", err);
+    }
 }
 
 function getDB() {
-  return database;
+    return database;
 }
 
 module.exports = { connectDB, getDB };
