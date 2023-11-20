a04f5565edfe69ca1d39e788b6bc8ebb53523308
root <root@ip-172-31-1-138.ap-southeast-2.compute.internal>
2023-11-20 10:40:30 +0000

    fix: .gitignore + remove ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
             "PORT": 80,
             "NODE_ENV": "development",
 		"JWT_SECRET": "SECRET",
-		  "OPENAI_APIKEY": "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",
+		  "OPENAI_APIKEY": "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",
 		  "MONGODB_URI": "mongodb+srv://geryjuniarto99:Gwej0tKuMg7cxa7S@cluster0.2wckrxu.mongodb.net/?retryWrites=true&w=majority"
         }
   }]
