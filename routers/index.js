8129880ed1c69a0ba63882ef90ef9459177b3b45
GJuniarto <geryjuniarto99@gmail.com>
2023-11-20 13:36:53 +0700

    feat: Journal findbyId done

--- routers/index.js
+++ routers/index.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const JournalController = require("../controllers/JournalController");
 const QuoteController = require("../controllers/QuoteController");
 const RecordController = require("../controllers/RecordController");
 const UserController = require("../controllers/UserController");
@@ -14,5 +15,7 @@ router.get("/quotes", QuoteController.generateQuote);
 
 router.post("/records", RecordController.createRecord);
 router.get("/records", RecordController.getRecords);
+
+router.get("/journals/:id", JournalController.findById);
 
 module.exports = router;
